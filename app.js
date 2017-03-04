6757ffa515685817640ab2f48ce387c57a0acc69
amota511 <aaronmotayne@gmail.com>
2017-03-04 15:20:51 -0500

    Parallaxing esque effect working exactly as it should!
    Let’s get it!!

--- app.js
+++ app.js
@@ -1,44 +1,42 @@
 var main = function() {
 			
-			// var top = document.body.scrollTop;
-             var background_image = $("#full-screen-background-image")[0];
-   //          var img1 = "http://cdn.wonderfulengineering.com/wp-content/uploads/2014/01/HD-Technology-Wallpaper.jpg"; 
-   //          var img2 = "http://www.wallpapers10.net/wp-content/uploads/images/wallpapers10.net-abstract-143.jpg";
-            //document.body.scrollTop = 0;
+	document.body.scrollTop = 0;
 
-            window.onscroll = function() {
+    var background_image = $("#full-screen-background-image")[0];
+    var img1 = "http://cdn.wonderfulengineering.com/wp-content/uploads/2014/01/HD-Technology-Wallpaper.jpg"; 
+    var img2 = "http://www.wallpapers10.net/wp-content/uploads/images/wallpapers10.net-abstract-143.jpg";
+    var scrollBy = 0;
+    var lastScroll = 0;
+    var window_height = window.innerHeight;       
 
-                if (document.body.scrollTop > 600 && background_image.src != "http://www.wallpapers10.net/wp-content/uploads/images/wallpapers10.net-abstract-143.jpg") {
-                     background_image.src = "http://www.wallpapers10.net/wp-content/uploads/images/wallpapers10.net-abstract-143.jpg";
-                     background_image.style.left = 0 + 'px';
-                     console.log("The second image is now visible");
-                }  
-                if (document.body.scrollTop < 600 && background_image.src != "http://cdn.wonderfulengineering.com/wp-content/uploads/2014/01/HD-Technology-Wallpaper.jpg") {
-                    background_image.src = "http://cdn.wonderfulengineering.com/wp-content/uploads/2014/01/HD-Technology-Wallpaper.jpg";
-                    background_image.style.left = 0 + 'px';
-                     console.log("The first image is now visible");
-                }
-                background_image.style.left = (document.body.scrollTop * 1.69) + 'px';
-                // $("#full-screen-background-image").animate({
-                // 	width: "70%",
-                // 	top: '+=0.1'
-                // },
-                // 10,
-                // function() {
-                // 	//completion block
-                // }); 
-                // background_image.style.left = (background_image.style.left + 20) + 'px';
-                // console.log(background_image.style.left);
-            };
-       //This works
-      //$('#full-screen-background-image')[0].src = "http://www.wallpapers10.net/wp-content/uploads/images/wallpapers10.net-abstract-143.jpg";
-      //console.log($('#full-screen-background-image').prop('src'));
+    window.onscroll = function() {
+    	
+    	var top = document.body.scrollTop;
 
-    // window.onResize = function(event) {
-    // 	console.log(window.innerHeight);
-    // }
+        if (top > window_height) {
+        	if (background_image.src != img2){
+        		background_image.src = img2;
+            	console.log("The second image is now visible");
+        	}
+        	background_image.style.top = (-(top - (window_height * 2.2)) * 0.075) + 'px';
+            
+        }  else if (top < window_height) {
+        	if (background_image.src != img1) {
+        		background_image.src = img1;
+            	console.log("The first image is now visible");
+        	}
+            background_image.style.top = -(top * 0.075) + 'px';
+        }
+        
+        console.log(-(top - window_height));
+    };
 
-    window.addEventListener('resize', function(){console.log(1);}, true);
+    window.addEventListener('resize', 
+    	function(){
+    		console.log(1);
+    		window_height = window.innerHeight;
+    	}, true);
+
 };
 
 $(document).ready(main);
