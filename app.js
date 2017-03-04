79a089c6a6cfacb7d4bfda6516671043381b20d2
amota511 <aaronmotayne@gmail.com>
2017-03-04 13:49:38 -0500

    Updated background image functionality, size and position of the first images text is set
    Let’s go!

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 var main = function() {
 			
 			// var top = document.body.scrollTop;
-   //          var background_image = $("#full-screen-background-image");
+             var background_image = $("#full-screen-background-image")[0];
    //          var img1 = "http://cdn.wonderfulengineering.com/wp-content/uploads/2014/01/HD-Technology-Wallpaper.jpg"; 
    //          var img2 = "http://www.wallpapers10.net/wp-content/uploads/images/wallpapers10.net-abstract-143.jpg";
             //document.body.scrollTop = 0;
 
             window.onscroll = function() {
 
-                // if (document.body.scrollTop > 600 && $("#full-screen-background-image").src != "http://www.wallpapers10.net/wp-content/uploads/images/wallpapers10.net-abstract-143.jpg") {
-                //      $("#full-screen-background-image").src = "http://www.wallpapers10.net/wp-content/uploads/images/wallpapers10.net-abstract-143.jpg";
-                //      console.log("The second image is now visible");
-                // }  
-                // if (document.body.scrollTop < 600 && $("#full-screen-background-image").src != "http://cdn.wonderfulengineering.com/wp-content/uploads/2014/01/HD-Technology-Wallpaper.jpg") {
-                //     
-                //      console.log("The first image is now visible");
-                // }
+                if (document.body.scrollTop > 600 && background_image.src != "http://www.wallpapers10.net/wp-content/uploads/images/wallpapers10.net-abstract-143.jpg") {
+                     background_image.src = "http://www.wallpapers10.net/wp-content/uploads/images/wallpapers10.net-abstract-143.jpg";
+                     console.log("The second image is now visible");
+                }  
+                if (document.body.scrollTop < 600 && background_image.src != "http://cdn.wonderfulengineering.com/wp-content/uploads/2014/01/HD-Technology-Wallpaper.jpg") {
+                    background_image.src = "http://cdn.wonderfulengineering.com/wp-content/uploads/2014/01/HD-Technology-Wallpaper.jpg";
+                     console.log("The first image is now visible");
+                }
                 
-                // background_image.animate({
-                //	width: "70%",
+                // $("#full-screen-background-image").animate({
+                // 	width: "70%",
                 // 	top: '+=0.1'
                 // },
                 // 10,
                 // function() {
                 // 	//completion block
                 // }); 
-                
-                
+                // background_image.style.left = (background_image.style.left + 20) + 'px';
+                // console.log(background_image.style.left);
             };
        //This works
-      document.getElementById('full-screen-background-image').src = "http://www.wallpapers10.net/wp-content/uploads/images/wallpapers10.net-abstract-143.jpg";
+      //$('#full-screen-background-image')[0].src = "http://www.wallpapers10.net/wp-content/uploads/images/wallpapers10.net-abstract-143.jpg";
       //console.log($('#full-screen-background-image').prop('src'));
 
     // window.onResize = function(event) {
